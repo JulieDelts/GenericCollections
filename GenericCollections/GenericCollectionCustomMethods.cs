fd391788eb43fbe9b436e044fda6ba683c57206e
JulieDelts <juliadelcova@gmail.com>
2024-04-12 12:11:35 +0300

    Add project files.

---
+++ GenericCollections/GenericCollectionCustomMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    public static class GenericCollectionCustomMethods
+    {
+        //23.Обзор коллекций
+        public static double GetMaxScore(List<double> scores)
+        {
+            double maxScore = 0;
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (scores[i] > maxScore) maxScore = scores[i];
+            }
+            return maxScore;
+        }
+        public static List<double> CalculatePricesWithMarkup(List<double> prices, double markup)
+        {
+            List<double> pricesWithMarkup = new List<double>(prices.Count);
+            for (int i = 0; i < prices.Count; i++)
+            {
+                pricesWithMarkup.Add(prices[i] + prices[i] * markup / 100);
+            }
+            return pricesWithMarkup;
+        }
+        public static int CountUnderachievingClerks(List<double> results, double requiredSum)
+        {
+            int count = 0;
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (results[i] < requiredSum) count++;
+            }
+            return count;
+        }
+
+        public static void Print(List<double> list)
+        { 
+            foreach (var item in list) 
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
---
+++ GenericCollections/Program.cs
@@ -0,0 +1,15 @@
+﻿//использование методов
+using GenericCollections;
+
+List<double> scores = new List<double>() { 2.3, 4.5, 1.1, 5.6, 3.8, 4.7 };
+double maxScore = GenericCollectionCustomMethods.GetMaxScore(scores);
+Console.WriteLine($"Наивысший рейтинг - {maxScore}");
+
+
+List<double> prices = new List<double>() { 23, 10, 15.5, 42, 12};
+List<double> pricesWithMarkup = GenericCollectionCustomMethods.CalculatePricesWithMarkup(prices, 18);
+GenericCollectionCustomMethods.Print(pricesWithMarkup);
+
+List<double> salesResults = new List<double>() { 32, 12.5, 25.5, 18, 29.3, 30, 15, 33.3 };
+int failedClerks = GenericCollectionCustomMethods.CountUnderachievingClerks(salesResults, 20);
+Console.WriteLine($"Количество работников, не выполнивших норму -  {failedClerks} ");
